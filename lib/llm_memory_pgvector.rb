d8f13947a674072121a7fc1b60543dbd19652e3d
Shohei Kameda <shoheik@cpan.org>
2023-05-16 21:12:33 +0900

    first

---
+++ lib/llm_memory_pgvector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "llm_memory_pgvector/version"
+
+module LlmMemoryPgvector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/llm_memory_pgvector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LlmMemoryPgvector
+  VERSION = "0.1.0"
+end
